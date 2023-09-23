acda38615ddf22497cd442fd0a23e4dd922d0c90
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-23 10:39:38 +0200

    change DOM from js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+const questionText = 'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+const rightAnswer = 'Bell';
+const question = document.querySelector('.question');
+question.textContent = questionText;
+const answerText = prompt(questionText);
+const answer = document.querySelector('.answer');
+answer.textContent = answerText;
+if (answerText.toLowerCase().trim() === rightAnswer.toLowerCase()) {
+    answer.classList.add('answer--correct');
+} else {
+    answer.classList.add('answer--wrong');
+}
